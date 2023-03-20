c204023a4ba3e38c859d3cd75f67b5ff0d72eca3
GolovanovAlex <Golovanov.a.a@yandex.ru>
2023-03-20 21:35:53 +0300

    js wow animated

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,16 @@
-$(function () {});
+// ---- ---- scroll  ---- ---- //
+const anchors = document.querySelectorAll('a[href*="#"]');
+for (let anchor of anchors) {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    const blockID = anchor.getAttribute('href').substr(1);
+
+    document.getElementById(blockID).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start',
+    });
+  });
+}
+// ---- ---- wow.js ---- ---- //
+new WOW().init();
